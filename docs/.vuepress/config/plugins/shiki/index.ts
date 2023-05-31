7cfe35c76bc8d0a7d36c194855580339c20f14f6
MonoLogueChi <xxwhite.com@gmail.com>
2023-05-31 15:59:00 +0800

    更新包

--- docs/.vuepress/config/plugins/shiki/index.ts
+++ docs/.vuepress/config/plugins/shiki/index.ts
@@ -1,5 +1,5 @@
 import { shikiPlugin } from "@vuepress/plugin-shiki";
-import { BUNDLED_LANGUAGES } from "shiki";
+import { langs } from "./langs";
 
 import caddyfile from "./languages/caddyfile.tmLanguage.json" assert { type: "json" };
 
@@ -10,11 +10,9 @@ const caddyfileLanguage = {
   aliases: ["caddyfile", "caddy"],
 };
 
-// @ts-ignore
-BUNDLED_LANGUAGES.push(caddyfileLanguage);
-
 export default shikiPlugin({
   // 你的选项
   theme: "vitesse-light",
-  langs: BUNDLED_LANGUAGES,
+  //@ts-ignore
+  langs: [...langs, caddyfileLanguage],
 });
