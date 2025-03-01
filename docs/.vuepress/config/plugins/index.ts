e02a8772c1ffbda61035d16033ec15a92cdc4044
MonoLogueChi <xxwhite@foxmail.com>
2025-03-01 16:56:06 +0800

    更新文档

--- docs/.vuepress/config/plugins/index.ts
+++ docs/.vuepress/config/plugins/index.ts
@@ -2,10 +2,12 @@ import type { PluginConfig } from "vuepress";
 import copyjs from "./copyjs.js";
 import sbaudio from "vuepress-plugin-sbaudio";
 
-
 export default <PluginConfig>[
   copyjs,
   sbaudio({
+    aplayerGlobalOptions: {
+      order: "random",
+    },
     metingOptions: {
       api: "https://meting-api.u2sb.com/?server=:server&type=:type&id=:id&r=:r",
       list: [
--- docs/.vuepress/config/plugins/shiki/index.ts
+++ docs/.vuepress/config/plugins/shiki/index.ts
@@ -1,6 +1,5 @@
 import {
   ShikiPluginOptions,
-  bundledLanguageNames,
 } from "@vuepress/plugin-shiki";
 
 import caddyfile from "./languages/caddyfile.tmLanguage.json" with { type: 'json'};
@@ -12,12 +11,11 @@ const caddyfileLanguage = {
 };
 
 export default <ShikiPluginOptions>{
-  // 你的选项
   themes: {
     light: "one-light",
     dark: "one-dark-pro",
   },
   lineNumbers: true,
 
-  langs: [caddyfileLanguage, ...bundledLanguageNames],
+  langs: [caddyfileLanguage],
 };
